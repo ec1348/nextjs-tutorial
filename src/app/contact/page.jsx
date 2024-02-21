e9beba049946e22f1c62afc3fc89d6d3ac6cd772
Eric Chen <ec1348@gmail.com>
2024-02-21 14:26:21 +0800

    styling contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,21 @@
+import Image from "next/image"
+import styles from "./contact.module.css"
 const ContactPage = () => {
     return(
-        <div>ContactPage</div>
+        <div className={styles.container}>
+            <div className={styles.imgContainer}>
+                <Image src="/contact.png" alt="" fill  className={styles.img}/>
+            </div>           
+             <div className={styles.formContainer}>
+                <form action="" className={styles.form}>
+                    <input type="text" placeholder="Name"/>
+                    <input type="text" placeholder="Email address"/>
+                    <input type="text" placeholder="Phone number"/>
+                    <textarea name="" id="" cols="30" rows="10" placeholder="Message"></textarea>
+                    <button>Submit</button>
+                </form>
+             </div>
+        </div>
     )
 }
 
